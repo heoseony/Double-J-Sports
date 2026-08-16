59fe9ef2a834280021f610acd9dce0ad9643bd44
sunny <heungeol@gmail.com>
2026-08-16 22:18:10 +0200

    add PWA manifest and icons

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,22 @@
-import "./globals.css";
+﻿import "./globals.css";
 
 export const metadata = {
   title: "Double J Sports",
   description: "Double J Sports 회원관리 및 수업예약 시스템",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icon-192.png",
+    apple: "/icon-192.png",
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Double J Sports",
+  },
+};
+
+export const viewport = {
+  themeColor: "#0b3d2e",
 };
 
 export default function RootLayout({ children }) {
